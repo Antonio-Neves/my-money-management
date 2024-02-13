0889df5197d920e3f709251de7fa2d6cbf5e5411
antonioneves <antoniocalheirosneves@hotmail.com>
2024-02-13 17:24:31 +0000

    Added list with months

--- principal/views.py
+++ principal/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+months_list = [
+	'Janeiro', 'Fevereiro', 'Março', 'abril',
+	'maio', 'junho', 'julho', 'agosto',
+	'setembro', 'outubro', 'novembro', 'dezembro'
+]
 
 # --- Home Page --- #
 class IndexView(TemplateView):
 	template_name = 'principal/index.html'
+	extra_context = {'months_list': months_list}
