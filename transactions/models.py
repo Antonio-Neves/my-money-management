dd26eea9048b203ecb54ca351b7df9564ff66c05
antonioneves <antoniocalheirosneves@hotmail.com>
2024-02-24 18:49:28 +0000

    Created model entrada

--- transactions/models.py
+++ transactions/models.py
@@ -30,3 +30,33 @@ class MetodoTransacao(models.Model):
 
     def get_absolute_url(self):
         return reverse("metodo-transacao", kwargs={"pk": self.pk})
+
+
+class Entrada(models.Model):
+    entrada_id = models.BigAutoField(
+        'ID - Entrada', primary_key=True
+    )
+    entrada_data = models.DateField(
+        'Data', auto_now=False, auto_now_add=False
+    )
+    entrada_ds = models.CharField(
+        'Descrição', max_length=100
+    )
+    entrada_valor = models.DecimalField(
+        'Valor', max_digits=7, decimal_places=2
+    )
+    entrada_metodo_transacao = models.ForeignKey(
+        'MetodoTransacao', default='Dinheiro',
+        on_delete=models.SET_DEFAULT
+    )
+
+    class Meta:
+        verbose_name = 'Entrada'
+        verbose_name_plural = 'Entradas'
+        ordering = ['entrada_data', 'entrada_id']
+
+    def __str__(self):
+        return f'{self.metodo_transacao_nome}'
+
+    def get_absolute_url(self):
+        return reverse("entrada", kwargs={"pk": self.pk})
