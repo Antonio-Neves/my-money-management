e3903d805a04b8b5ca17f43a1874ac9afcabc65d
antonioneves <antoniocalheirosneves@hotmail.com>
2024-02-24 18:48:53 +0000

    Created model metodo transacao

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+METODO_TRANSACAO_TIPO_CHOICES = {
+    'E': 'Eletrônico',
+    'F': 'Físico'
+}
+
+
+class MetodoTransacao(models.Model):
+    metodo_transacao_id = models.BigAutoField(
+        'ID - Método Transação', primary_key=True
+    )
+    metodo_transacao_nome = models.CharField(
+        'Método de Transação', max_length=100, unique=True
+    )
+    metodo_transacao_tipo = models.CharField(
+        'Tipo de transação', max_length=1,
+        choices=METODO_TRANSACAO_TIPO_CHOICES
+    )
+
+    class Meta:
+        verbose_name = 'Método de transação'
+        verbose_name_plural = 'Métodos de transação'
+        ordering = ['metodo_transacao_nome']
+
+    def __str__(self):
+        return f'{self.metodo_transacao_nome}'
+
+    def get_absolute_url(self):
+        return reverse("metodo-transacao", kwargs={"pk": self.pk})
