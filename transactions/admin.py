b1682bfc378178ac0b4a11ddd552a1766708d806
antonioneves <antoniocalheirosneves@hotmail.com>
2024-02-25 11:58:27 +0000

    Added metodotransacao model to admin

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from transactions.models import MetodoTransacao, Entrada
+
+
+@admin.register(MetodoTransacao)
+class MetodoTransacaoAdmin(admin.ModelAdmin):
+
+	list_display = (
+		'metodo_transacao_nome',
+		'metodo_transacao_tipo'
+	)
+
+	exclude = ['metodo_transacao_usuario']
+
+	def get_queryset(self, request):
+
+		qs = super().get_queryset(request)
+		return qs.filter(metodo_transacao_usuario=request.user)
+
+	def save_model(self, request, obj, form, change):
+
+		obj.metodo_transacao_usuario = request.user
+		super().save_model(request, obj, form, change)
