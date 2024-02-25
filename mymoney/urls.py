ef298027849a4d997bb70c28e6ced95734ea7528
antonioneves <antoniocalheirosneves@hotmail.com>
2024-02-25 11:59:28 +0000

    Changed target to env variable

--- mymoney/urls.py
+++ mymoney/urls.py
@@ -18,9 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from decouple import config
-
-ADMIN_SITE = config('ADMIN_SITE')
+from settings import ADMIN_SITE
 
 
 urlpatterns = [
