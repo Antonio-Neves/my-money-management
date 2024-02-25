63647b8e89cd4f4d8a9f7611ac64eb66788c696e
antonioneves <antoniocalheirosneves@hotmail.com>
2024-02-25 16:27:53 +0000

    changed imports from settings file

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import Group
 
 from accounts.models import CustomUser
-from settings import CUSTOM_USER_GROUP
+from mymoney.settings import CUSTOM_USER_GROUP
 
 
 class CustomUserCreateForm(UserCreationForm):
--- mymoney/urls.py
+++ mymoney/urls.py
@@ -18,8 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from settings import ADMIN_SITE
-
+from mymoney.settings import ADMIN_SITE
 
 urlpatterns = [
 	path('accounts/', include('accounts.urls')),
