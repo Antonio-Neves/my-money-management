480e397b90b43d97347996c3dac9b7c6d8b7ceb2
antonioneves <antoniocalheirosneves@hotmail.com>
2024-03-23 15:53:08 +0000

    Added debug toolbar

--- mymoney/settings.py
+++ mymoney/settings.py
@@ -35,21 +35,26 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 # Allowed Hosts
 # --- development --- #
 if DEBUG:
-	ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = []
 
 # --- Production --- #
 if not DEBUG:
-	ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
+    ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
 
 
 # ----------------------------------------------------------
 # SSL and Cookies
 # ----- Production ----- #
 if not DEBUG:
-	# SECURE_SSL_REDIRECT = True
-	SESSION_COOKIE_SECURE = True
-	CSRF_COOKIE_SECURE = True
-	CSRF_TRUSTED_ORIGINS = [config('TRUSTED_ORIGINS')]
+    # SECURE_SSL_REDIRECT = True
+    ADMINS = [(config('SUPER_USER'), config('EMAIL'))]
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    CSRF_TRUSTED_ORIGINS = [config('TRUSTED_ORIGINS')]
+
+# Django Debug toolbar, only in development
+if DEBUG:
+    INTERNAL_IPS = ['127.0.0.1']
 
 
 # ----------------------------------------------------------
@@ -62,35 +67,43 @@ CUSTOM_USER_GROUP = config('CUSTOM_USER_GROUP')
 # Application definition
 
 INSTALLED_APPS = [
-	# --- Accounts --- #
-	'accounts',
+    # --- Accounts --- #
+    'accounts',
 
-	# --- Django Apps --- #
-	'django.contrib.admin',
-	'django.contrib.auth',
-	'django.contrib.contenttypes',
-	'django.contrib.sessions',
-	'django.contrib.messages',
-	'django.contrib.staticfiles',
+    # --- Django Apps --- #
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
 
-	# --- system apps --- #
-	'base',
-	'principal',
-	'transactions'
+    # --- system apps --- #
+    'base',
+    'principal',
+    'transactions'
 ]
+
+# Django Debug toolbar, only in development
+if DEBUG:
+    INSTALLED_APPS.append('debug_toolbar')
 
 
 # ----------------------------------------------------------
 MIDDLEWARE = [
-	'django.middleware.security.SecurityMiddleware',
-	'whitenoise.middleware.WhiteNoiseMiddleware',
-	'django.contrib.sessions.middleware.SessionMiddleware',
-	'django.middleware.common.CommonMiddleware',
-	'django.middleware.csrf.CsrfViewMiddleware',
-	'django.contrib.auth.middleware.AuthenticationMiddleware',
-	'django.contrib.messages.middleware.MessageMiddleware',
-	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# Django Debug toolbar, only in development
+if DEBUG:
+    MIDDLEWARE.insert(2, 'debug_toolbar.middleware.DebugToolbarMiddleware')
 
 
 # ----------------------------------------------------------
@@ -99,19 +112,19 @@ ROOT_URLCONF = 'mymoney.urls'
 
 # ----------------------------------------------------------
 TEMPLATES = [
-	{
-		'BACKEND': 'django.template.backends.django.DjangoTemplates',
-		'DIRS': [],
-		'APP_DIRS': True,
-		'OPTIONS': {
-			'context_processors': [
-				'django.template.context_processors.debug',
-				'django.template.context_processors.request',
-				'django.contrib.auth.context_processors.auth',
-				'django.contrib.messages.context_processors.messages',
-			],
-		},
-	},
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
 
 
@@ -124,11 +137,11 @@ WSGI_APPLICATION = 'mymoney.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-	'default': dj_database_url.config(
-		default = config('DATABASE_URL'),
-		conn_max_age=600,
-		conn_health_checks=True,
-	),
+    'default': dj_database_url.config(
+        default = config('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
 }
 
 
@@ -137,18 +150,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-	{
-		'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-	},
-	{
-		'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-	},
-	{
-		'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-	},
-	{
-		'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-	},
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
 ]
 
 
@@ -156,7 +169,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-pt'
 
 TIME_ZONE = 'Europe/Lisbon'
 
@@ -181,19 +195,19 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # --- development --- #
 if DEBUG:
-	EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # --- Production --- #
 if not DEBUG:
-	EMAIL_HOST = config('EMAIL_HOST')
-	EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-	EMAIL_PORT = config('EMAIL_PORT', cast=int)
-	EMAIL_USE_SSL = True
-	EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-	DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+    EMAIL_HOST = config('EMAIL_HOST')
+    EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+    EMAIL_PORT = config('EMAIL_PORT', cast=int)
+    EMAIL_USE_SSL = True
+    EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+    DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
-	ADMINS = [(config('SUPER_USER'), config('EMAIL'))]
-	MANAGERS = ADMINS
+    ADMINS = [(config('SUPER_USER'), config('EMAIL'))]
+    MANAGERS = ADMINS
 
 
 # ----------------------------------------------------------
@@ -211,11 +225,11 @@ LOGOUT_REDIRECT_URL = 'index'
 # ----------------------------------------------------------
 # Mensagens
 MESSAGE_TAGS = {
-	constants.ERROR: 'alert-danger',
-	constants.WARNING: 'alert-warning',
-	constants.DEBUG: 'alert-info',
-	constants.SUCCESS: 'alert-success',
-	constants.INFO: 'alert-info',
+    constants.ERROR: 'alert-danger',
+    constants.WARNING: 'alert-warning',
+    constants.DEBUG: 'alert-info',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
 }
 
 
