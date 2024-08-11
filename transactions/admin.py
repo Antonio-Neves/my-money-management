b1d8e8b5f6dd96474e84865f2c9450d0257f77f3
Antonio Neves <antoniocalheirosneves@hotmail.com>
2024-08-11 18:18:26 +0100

    Registered new tables in admin

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,16 +1,15 @@
 from django.contrib import admin
 
+
 from transactions.models import (
-    MetodoTransacao,
     AreaTransacao,
     TipoTransacao,
-    Entrada,
-    Saida,
-
+    MetodoTransacao,
+    Transacao,
 )
 
 
-@admin.register(MetodoTransacao, AreaTransacao, TipoTransacao)
+@admin.register(AreaTransacao, TipoTransacao, MetodoTransacao)
 class FilterUserAdmin(admin.ModelAdmin):
 
     exclude = ['user_connected']
@@ -26,49 +25,26 @@ class FilterUserAdmin(admin.ModelAdmin):
         super().save_model(request, obj, form, change)
 
 
-@admin.register(Entrada)
-class EntradaAdmin(FilterUserAdmin):
+@admin.register(Transacao)
+class TransacaoAdmin(FilterUserAdmin):
 
     list_display = [
-        'entrada_data',
-        'entrada_area',
-        'entrada_ds',
-        'entrada_tipo',
-        'entrada_valor',
-        'entrada_metodo_transacao',
+        'transacao_data',
+        'transacao_area',
+        'transacao_descricao',
+        'transacao_valor',
+        'transacao_metodo',
+        'transacao_tipo',
+        'transacao_destino',
+        'transacao_entrada_saida'
     ]
 
-    list_filter = ['entrada_data', 'entrada_tipo']
+    list_filter = ['transacao_data', 'transacao_entrada_saida', 'transacao_tipo']
 
     def render_change_form(self, request, context, *args, **kwargs):
 
-        context['adminform'].form.fields['entrada_area'].queryset = AreaTransacao.objects.filter(user_connected=request.user)
-        context['adminform'].form.fields['entrada_tipo'].queryset = TipoTransacao.objects.filter(user_connected=request.user)
-        context['adminform'].form.fields['entrada_metodo_transacao'].queryset = MetodoTransacao.objects.filter(user_connected=request.user)
-
-        return super().render_change_form(request, context, args, kwargs)
-
-
-@admin.register(Saida)
-class SaidaAdmin(FilterUserAdmin):
-
-    list_display = [
-        'saida_data',
-        'saida_area',
-        'colored_saida_ds',
-        'colored_saida_tipo',
-        'saida_valor',
-        'saida_metodo_transacao',
-    ]
-
-    search_fields = ["saida_ds"]
-
-    list_filter = ['saida_data', 'saida_tipo']
-
-    def render_change_form(self, request, context, *args, **kwargs):
-
-        context['adminform'].form.fields['saida_area'].queryset = AreaTransacao.objects.filter(user_connected=request.user)
-        context['adminform'].form.fields['saida_tipo'].queryset = TipoTransacao.objects.filter(user_connected=request.user)
-        context['adminform'].form.fields['saida_metodo_transacao'].queryset = MetodoTransacao.objects.filter(user_connected=request.user)
+        context['adminform'].form.fields['transacao_area'].queryset = AreaTransacao.objects.filter(user_connected=request.user)
+        context['adminform'].form.fields['transacao_tipo'].queryset = TipoTransacao.objects.filter(user_connected=request.user)
+        context['adminform'].form.fields['transacao_metodo'].queryset = MetodoTransacao.objects.filter(user_connected=request.user)
 
         return super().render_change_form(request, context, args, kwargs)
