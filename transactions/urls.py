1bd408b307a1463178d079dc95da29ae5055dd8f
Antonio-Neves <antoniocalheirosneves@gmail.com>
2025-06-19 12:53:37 +0100

    Added empty urls list

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
 
+# from transactions.views import (
+#     CreateTransactionView,
+# )
+
 urlpatterns = [
-	# path('', ),
+	# path('create-transaction', CreateTransactionView.as_view(), name='create-transaction'),
 ]
