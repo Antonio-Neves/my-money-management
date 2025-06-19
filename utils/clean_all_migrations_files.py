f8d1af4114eb4226252d00e9c75f690df1a56b5e
Antonio-Neves <antoniocalheirosneves@gmail.com>
2025-06-19 12:50:41 +0100

    Activated code to eliminate migrations

--- utils/clean_all_migrations_files.py
+++ utils/clean_all_migrations_files.py
@@ -6,18 +6,19 @@
 # --------------------------------------------- #
 
 
-# import os
-#
-# def delete_initial_files(project_path, file_name):
-#
-#     for root, dirs, files in os.walk(project_path):
-#         if '.venv' not in root:
-#             for file in files:
-#                 if file.endswith(file_name):
-#                     os.remove(os.path.join(root, file))
-#                     print(f"Arquivo {file} deletado em {root}")
-#
-#
-# project_path = '..'
-#
-# delete_initial_files(project_path, '_initial.py')
+import os
+
+
+def delete_initial_files(project_path, file_name):
+
+    for root, dirs, files in os.walk(project_path):
+        if '.venv' not in root:
+            for file in files:
+                if file.endswith(file_name):
+                    os.remove(os.path.join(root, file))
+                    print(f"Arquivo {file} deletado em {root}")
+
+
+project_path = '..'
+
+delete_initial_files(project_path, '_initial.py')
